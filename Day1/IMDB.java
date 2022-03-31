56323d8ac44e2dbbecf0872da1b962ca45b41da0
Guilherme Procopio <71650069+GuilhermeProcopio@users.noreply.github.com>
2022-03-31 10:03:09 -0300

    Consume an api of IMDB

---
+++ Day1/IMDB.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class IMDB {
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+
+        HttpClient client = HttpClient
+                .newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+
+
+
+        HttpRequest request = HttpRequest
+                .newBuilder ()
+                .uri (new URI("https://imdb-api.com/en/API/Top250Movies/k_aaaaaaaa"))
+                .GET ()
+                .build();
+
+        HttpResponse<String>  response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
+    }
+}
